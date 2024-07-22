061bea558751976a804ac3dbe679ccebe1c18090
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-07-22 13:12:43 +0530

    common files and image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Homepage from '../src/Components/Pages/Homepage/Homepage'
+import About from '../src/Components/Pages/About/About'
+import Services from '../src/Components/Pages/Services/Services'
+import Contact from '../src/Components/Pages/Contact/Contact'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+ 
